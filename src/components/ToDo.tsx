9a1f9d879e7fe329c78c1d161f8a87604837c843
sark <sark@supersark.com>
2022-06-09 14:52:44 +0900

    5.13

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,11 +1,31 @@
+import React from "react";
 import { IToDo } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+  };
   return (
     <li>
-      <span>{text}</span> <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
